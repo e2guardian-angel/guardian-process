912076e800147886fd4fbec0942ec9a0ec601708
Justin Schwartzbeck <justinmschw@gmail.com>
2021-08-05 22:04:28 -0500

    Improve firewall configuration

--- app.js
+++ app.js
@@ -7,9 +7,24 @@ const p = new gProcess();
 
 let configWatcher = watch(p.configFile);
 
+// Initialize global firewall rules
+p.initFirewall();
+
 configWatcher.on('change', async function(evt, name) {
     console.log('Applying new config...');
     await p.update();
     console.log('Done');
 });
 
+async function gracefulShutdown() {
+    console.info('Shutting down...');
+    configWatcher.close();
+    console.info('Clearing firewall rules...');
+    await p.tearDownFirewall();
+    console.info('guardian-process exited.');
+    process.exit(0);
+}
+
+process.on('SIGINT', gracefulShutdown);
+process.on('SIGTERM', gracefulShutdown);
+process.on('SIGHUP', gracefulShutdown);
